e9c5927bad62dd994e94d8797be6e97fa0ced829
jennifershi255 <jennifershi255@gmail.com>
2025-01-02 00:09:49 -0500

    added comments

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -4,53 +4,52 @@ import im2 from "../assets/im2.jpg";
 import im3 from "../assets/im3.jpg";
 import im4 from "../assets/im4.jpg";
 import im5 from "../assets/im5.jpg";
-const UrgencySlider = (urgency) => {
-    console.log(urgency)
-    const [urgencyLevel, setUrgencyLevel] = useState(0); // Default level, e.g., Moderate
-    console.log(urgencyLevel)   
-    useEffect(() => {
-        if (urgency.urgency === "Trivial") {
-            setUrgencyLevel(0);
-        } else if (urgency.urgency === "Low Priority") {
-            setUrgencyLevel(1);
-        } else if (urgency.urgency === "Moderate") {
-            setUrgencyLevel(2);
-        } else if (urgency.urgency === "High Priority") {
-            setUrgencyLevel(3);
-        } else if (urgency.urgency === "Critical") {
-            setUrgencyLevel(4);
-        }
-    }, [urgency]);
-    // Urgency levels and their associated styles
-    const urgencyStyles = [
-      { label: "Trivial", bgColor: "bg-green-500", img: im1 },
-      { label: "Low Priority", bgColor: "bg-blue-500", img: im2 },
-      { label: "Moderate", bgColor: "bg-yellow-500", img: im3},
-      { label: "High Priority", bgColor: "bg-orange-500", img: im4},
-      { label: "Critical", bgColor: "bg-red-500", img: im5 },
-    ];
-  
-    return (
-      <div className="p-6 rounded-lg shadow-xl max-w-lg mx-auto bg-gray-900 text-white">
-        {/* Slider */}
-        <input
-          type="range"
-          min="0"
-          max="4"
-          value={urgencyLevel}
-          className={`slider w-full h-2 ${urgencyStyles[urgencyLevel].bgColor} rounded-full appearance-none`}
-        />
-  
-        {/* Urgency indicator */}
-        <div
-          className={`mt-6 h-16 rounded-lg transition duration-500 ease-in-out ${urgencyStyles[urgencyLevel].bgColor}`}
-        />
-       <img className="mx-auto" src={urgencyStyles[urgencyLevel].img} alt="" />
 
-      </div>
-    );
-  };
-  
+// Visually represents the urgency of the task
+const UrgencySlider = (urgency) => {
+  const [urgencyLevel, setUrgencyLevel] = useState(0); // Set default level to Trivial
+  useEffect(() => {
+    if (urgency.urgency === "Trivial") {
+      setUrgencyLevel(0);
+    } else if (urgency.urgency === "Low Priority") {
+      setUrgencyLevel(1);
+    } else if (urgency.urgency === "Moderate") {
+      setUrgencyLevel(2);
+    } else if (urgency.urgency === "High Priority") {
+      setUrgencyLevel(3);
+    } else if (urgency.urgency === "Critical") {
+      setUrgencyLevel(4);
+    }
+  }, [urgency]);
+
+  // Urgency levels and their associated styles
+  const urgencyStyles = [
+    { label: "Trivial", bgColor: "bg-green-500", img: im1 },
+    { label: "Low Priority", bgColor: "bg-blue-500", img: im2 },
+    { label: "Moderate", bgColor: "bg-yellow-500", img: im3 },
+    { label: "High Priority", bgColor: "bg-orange-500", img: im4 },
+    { label: "Critical", bgColor: "bg-red-500", img: im5 },
+  ];
+
+  return (
+    <div className="p-6 rounded-lg shadow-xl max-w-lg mx-auto bg-gray-900 text-white">
+      {/* Slider */}
+      <input
+        type="range"
+        min="0"
+        max="4"
+        value={urgencyLevel}
+        className={`slider w-full h-2 ${urgencyStyles[urgencyLevel].bgColor} rounded-full appearance-none`}
+      />
+
+      {/* Urgency indicator */}
+      <div
+        className={`mt-6 h-16 rounded-lg transition duration-500 ease-in-out ${urgencyStyles[urgencyLevel].bgColor}`}
+      />
+      <img className="mx-auto" src={urgencyStyles[urgencyLevel].img} alt="" />
+    </div>
+  );
+};
 
 const Form = () => {
   const [task, setTask] = useState("");
@@ -70,7 +69,6 @@ const Form = () => {
       body: JSON.stringify({
         task: task,
         type_of_res: type,
-        // mood: mood,
       }),
     })
       .then((res) => res.json())
@@ -84,16 +82,18 @@ const Form = () => {
       });
   }
 
-//   results = {response: "I'm sorry, I can't do that Dave.", urgency: "Critical"};
+  //   results = {response: "I'm sorry, I can't do that Dave.", urgency: "Critical"};
 
   return (
-    <div >
-      <form 
+    <div>
+      <form
         className="bg-gray-800 p-10 rounded-lg shadow-lg space-y-4 border-2 border-gray-600 "
         onSubmit={submit}
       >
         <div className="flex flex-col">
-          <label htmlFor="task" className="mb-2 font-bold">Task:</label>
+          <label htmlFor="task" className="mb-2 font-bold">
+            Task:
+          </label>
           <input
             type="text"
             id="task"
@@ -105,19 +105,29 @@ const Form = () => {
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="type" className="mb-2 font-bold">Type of Excuse:</label>
+          <label htmlFor="type" className="mb-2 font-bold">
+            Type of Excuse:
+          </label>
           <select
-          id="type"
-          placeholder="Enter the type of excuse you'd like..."
-          value={type} // Correct the value prop
-          onChange={(e) => setType(e.target.value)} // Correct the state setter
-          className="p-2 rounded bg-gray-700 focus:ring-2 focus:ring-indigo-500 outline-none"
-        >
-          <option className="text-black" value="">Select a type</option>
-          <option className="text-black" value="absurd">Absurd</option>
-          <option className="text-black" value="scientific">Scientific</option>
-          <option className="text-black" value="emotional">Emotional</option>
-        </select>
+            id="type"
+            placeholder="Enter the type of excuse you'd like..."
+            value={type} // Correct the value prop
+            onChange={(e) => setType(e.target.value)} // Correct the state setter
+            className="p-2 rounded bg-gray-700 focus:ring-2 focus:ring-indigo-500 outline-none"
+          >
+            <option className="text-black" value="">
+              Select a type
+            </option>
+            <option className="text-black" value="absurd">
+              Absurd
+            </option>
+            <option className="text-black" value="scientific">
+              Scientific
+            </option>
+            <option className="text-black" value="emotional">
+              Emotional
+            </option>
+          </select>
         </div>
 
         <button
@@ -130,23 +140,24 @@ const Form = () => {
 
       {/* Loading State */}
       {loading && (
-        <div className="mt-8 text-lg font-bold animate-pulse">
-          Loading...
-        </div>
+        <div className="mt-8 text-lg font-bold animate-pulse">Loading...</div>
       )}
 
       {/* Display Results after API call */}
       {results && (
         <div>
-        <div className="mt-8 bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-lg border-2 border-gray-600 ">
-          {/* <h3 className="text-xl font-bold mb-4">Returned Data:</h3> */}
-          <p className="text-lg">{results.response}</p>
-        </div>
-        <p className="text-4xl font-bold mt-4 mb-5 text-shadow text-center">
-          urgency of task: <span className="text-decoration-underline underline">{results.urgency}</span>
-        </p>
+          <div className="mt-8 bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-lg border-2 border-gray-600 ">
+            {/* <h3 className="text-xl font-bold mb-4">Returned Data:</h3> */}
+            <p className="text-lg">{results.response}</p>
+          </div>
+          <p className="text-4xl font-bold mt-4 mb-5 text-shadow text-center">
+            urgency of task:{" "}
+            <span className="text-decoration-underline underline">
+              {results.urgency}
+            </span>
+          </p>
 
-        <UrgencySlider urgency={results.urgency} />
+          <UrgencySlider urgency={results.urgency} />
         </div>
       )}
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import Hero from '../components/Hero';
-import Features from '../components/Features';
-import Footer from '../components/Footer';
-import star from "../assets/star.png";
-import Rocket from "../components/Rocket";
-
+import React, { useEffect, useState } from "react";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
 
 // Background image style
 const backgroundImageStyle = {
   backgroundImage: `url('https://static.vecteezy.com/system/resources/previews/036/223/015/large_2x/ai-generated-universe-galaxy-wallpaper-star-particle-motion-on-black-background-starlight-nebula-in-galaxy-at-universe-space-background-ai-generated-free-photo.jpg')`,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  minHeight: '100vh',
-  width: '100%',
-  overflow: 'hidden',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  minHeight: "100vh",
+  width: "100%",
+  overflow: "hidden",
 };
 
 function Home() {
-
   return (
     <div style={backgroundImageStyle}>
-      <header className="py-6 px-10 flex justify-between items-center">
-      </header>
+      <header className="py-6 px-10 flex justify-between items-center"></header>
       <main className="flex-grow">
         <Hero />
         <Features />
       </main>
-      
+
       <Footer />
     </div>
   );
 }
-
 
 export default Home;
